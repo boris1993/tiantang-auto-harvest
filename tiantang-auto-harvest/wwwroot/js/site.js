fb0aae215dce8ae11942b0dcfae5f66aa67275f5
Boris Zhao <boris1993@live.cn>
2022-03-01 23:10:32 +0800

    Refactor the web page (#23)
    Refactor the web page with Bootstrap

--- tiantang-auto-harvest/wwwroot/js/site.js
+++ tiantang-auto-harvest/wwwroot/js/site.js
@@ -1,4 +1,158 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+function sendSMSCode() {
+    let phoneNumber = document.getElementById("phone_number").value;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/SendSMS", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({
+        phoneNumber: phoneNumber
+    }));
 
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 400) {
+                let responseJson = JSON.parse(xhr.responseText);
+                let errorMessages = responseJson["errors"]["PhoneNumber"];
+
+                let alertMessage = "";
+                for (let i = 0; i < errorMessages.length; i++) {
+                    alertMessage += errorMessages[i] + "\n";
+                }
+
+                alert(alertMessage);
+            } else if (xhr.status !== 200) {
+                let alertMessage = "服务器出现错误：\n" + xhr.responseText;
+                alert(alertMessage);
+            }
+        }
+    }
+}
+
+function verifySMSCode() {
+    let phoneNumber = document.getElementById("phone_number").value;
+    let smsCode = document.getElementById("sms_code").value;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/VerifyCode", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({
+        phoneNumber: phoneNumber,
+        otpCode: smsCode
+    }));
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 400) {
+                let alertMessage = "";
+
+                let responseJson = JSON.parse(xhr.responseText);
+                let errorMessages = responseJson["errors"]["PhoneNumber"];
+                for (let i = 0; i < errorMessages.length; i++) {
+                    alertMessage += errorMessages[i] + "\n";
+                }
+
+                errorMessages = responseJson["errors"]["OTPCode"];
+                for (let i = 0; i < errorMessages.length; i++) {
+                    alertMessage += errorMessages[i] + "\n";
+                }
+
+                alert(alertMessage);
+
+                return;
+            } else if (xhr.status !== 200) {
+                let alertMessage = "服务器出现错误：\n" + xhr.responseText;
+                alert(alertMessage);
+
+                return;
+            }
+
+            window.location.reload();
+        }
+    }
+}
+
+function getCurrentLogin() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/api/GetLoginInfo", true);
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            let responseText = xhr.responseText;
+            if (xhr.status !== 200) {
+                let alertMessage = "服务器出现错误：\n" + responseText;
+                alert(alertMessage);
+                return;
+            }
+
+            if (responseText) {
+                let responseJson = JSON.parse(responseText);
+                document.getElementById("current_phone_number").value = responseJson["phoneNumber"];
+            }
+        }
+    }
+}
+
+function updateNotificationKeys() {
+    let serverChanSendKey = document.getElementById("server_chan_send_key").value;
+    let barkToken = document.getElementById("bark_token").value;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/UpdateNotificationChannels", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({
+        ServerChan: serverChanSendKey,
+        Bark: barkToken
+    }));
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status !== 200) {
+                let alertMessage = "服务器出现错误：\n" + xhr.responseText;
+                alert(alertMessage);
+                return;
+            }
+
+            window.location.reload();
+        }
+    }
+}
+
+function testNotificationKeys() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/api/TestNotificationChannels", true);
+    xhr.send();
+}
+
+function loadNotificationKeys() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "/api/GetNotificationKeys", true);
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            let responseText = xhr.responseText;
+            if (xhr.status !== 200) {
+                let alertMessage = "服务器出现错误：\n" + responseText;
+                alert(alertMessage);
+                return;
+            }
+
+            if (responseText) {
+                let responseJson = JSON.parse(responseText);
+
+                let serverChanSendKey = responseJson["serverChan"];
+                let barkToken = responseJson["bark"];
+
+                document.getElementById("server_chan_send_key").value = serverChanSendKey;
+                document.getElementById("bark_token").value = barkToken;
+            }
+        }
+    }
+}
+
+window.onload = function () {
+    getCurrentLogin();
+    loadNotificationKeys();
+}
 // Write your JavaScript code.
